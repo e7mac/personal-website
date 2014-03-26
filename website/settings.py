9ccfcf9dedd8665a449b55fa0186f4f2fea7cebe
e7mac <mayank.ot@gmail.com>
2014-03-25 22:57:58 -0700

    fix for static files served through gunicorn

--- website/settings.py
+++ website/settings.py
@@ -60,22 +60,12 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'home/e7mac/webapps/personal_static/'
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://e7mac.com/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), '../static/'),                      # Or path to 
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
